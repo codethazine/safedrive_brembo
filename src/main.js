4cd8567fdefa6331ba9d11da07a392b8926e0c15
“joanvelja” <Joan.velja@studbocconi.it>
2022-06-25 18:56:39 +0200

    added dashboard

--- src/main.js
+++ src/main.js
@@ -5,8 +5,14 @@ import store from './store'
 import Amplify from "aws-amplify";
 import AmplifyVue from '@aws-amplify/ui-vue';
 import awsconfig from './aws-exports';
+import "vue-material-design-icons/styles.css"
+import VueGoogleMaps from '@fawmi/vue-google-maps'
 
 
 Amplify.configure(awsconfig);
 
-createApp(App).use(store).use(router).use(AmplifyVue).mount('#app')
+createApp(App).use(store).use(router).use(AmplifyVue).use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyDZW-E-sugZ5z-hYSYPWLy4ThJs-HaSnXU',
+    },
+}).mount('#app')
